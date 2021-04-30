a153e76b3a8c45d8772b5571cc619348b60307db
Jeff Butler <jeffgbutler@gmail.com>
2021-04-30 17:32:57 -0400

    Redis Connection

---
+++ PaymentCalculator/Services/RedisHitCounterService.cs
@@ -0,0 +1,25 @@
+﻿using StackExchange.Redis;
+
+namespace PaymentCalculator.Services
+{
+    class RedisHitCounterService: IHitCounterService
+    {
+        private IConnectionMultiplexer _conn;
+        public RedisHitCounterService(IConnectionMultiplexer conn)
+        {
+            _conn = conn;
+        }
+
+        public long GetAndIncrement()
+        {
+            IDatabase db = _conn.GetDatabase();
+            return db.StringIncrement("payment-calculator");
+        }
+
+        public void Reset()
+        {
+            IDatabase db = _conn.GetDatabase();
+            db.StringSet("payment-calculator", 5000);
+        }
+    }
+}
--- PaymentCalculator/Startup.cs
+++ PaymentCalculator/Startup.cs
@@ -6,17 +6,20 @@ using Microsoft.Extensions.Hosting;
 using PaymentCalculator.Services;
 using Microsoft.OpenApi.Models;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Connector.Redis;
 
 namespace PaymentCalculator
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Env = env;
         }
 
         public IConfiguration Configuration { get; }
+        private IWebHostEnvironment Env;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -30,7 +33,17 @@ namespace PaymentCalculator
             services.AddOptions();
             services.AddSingleton<PaymentService>();
             services.AddSingleton<CrashService>();
-            services.AddSingleton<IHitCounterService, MemoryHitCounterService>();
+
+            if(Env.IsDevelopment())
+            {
+                services.AddSingleton<IHitCounterService, MemoryHitCounterService>();
+            }
+            else
+            {
+                services.AddRedisConnectionMultiplexer(Configuration);
+                services.AddSingleton<IHitCounterService, RedisHitCounterService>();
+            }
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
